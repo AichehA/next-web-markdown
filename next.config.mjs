33246c301dd2e4d4b961196d2b547aedb23c3a3c
Alexandre Aicheh <alexandre.aicheh@gmail.com>
2023-10-13 00:00:31 +0200

    Correcton de configuration

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-import { withContentlayer } from "next-contentlayer";
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-};
-
-export default withContentlayer(nextConfig);
